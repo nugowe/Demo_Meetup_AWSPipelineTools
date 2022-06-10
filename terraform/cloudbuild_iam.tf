9aefe79ac5089a99b0631e62ef232b2dac81f6ac
nugowe <nosa.ugowe@yahoo.com>
2022-06-10 11:32:16 -0500

    made changes to tf files

--- terraform/cloudbuild_iam.tf
+++ terraform/cloudbuild_iam.tf
@@ -148,9 +148,6 @@ data "aws_iam_role" "tf_iam_assumed_role" {
   name = "TerraformAssumedIamRole"
 }
 
-data "aws_iam_policy" "tf_iam_assumed_policy" {
-  name = "${aws_iam_policy.tf_iam_assumed_policy.arn}"
-}
 
 
 
@@ -159,5 +156,5 @@ data "aws_iam_policy" "tf_iam_assumed_policy" {
 # Attach IAM assume role to policy
 resource "aws_iam_role_policy_attachment" "tf_iam_attach_assumed_role_to_permissions_policy" {
   role       = data.aws_iam_role.tf_iam_assumed_role.name
-  policy_arn = data.aws_iam_policy.tf_iam_assumed_policy
+  policy_arn = aws_iam_policy.tf_iam_assumed_policy.arn
 }
