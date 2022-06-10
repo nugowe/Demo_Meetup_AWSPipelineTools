7aee0e45b8cccfca7ec4ee0b4c795f9f103d4d9d
nugowe <nosa.ugowe@yahoo.com>
2022-06-10 10:52:10 -0500

    made changes to iam tf files

--- terraform/codepipeline_iam.tf
+++ terraform/codepipeline_iam.tf
@@ -1,21 +1,4 @@
-#resource "aws_iam_role" "codepipeline_role" {
-  #name = "demo_meetup_codepipeline_role"
 
-  #assume_role_policy = <<EOF
-#{
-  #"Version": "2012-10-17",
-  "#Statement": [
-    #{
-      #"Effect": "Allow",
-      "#Principal": {
-        #"Service": "codepipeline.amazonaws.com"
-      #},
-      #"Action": "sts:AssumeRole"
-    #}
-  #]
-#}
-#EOF
-#}
 
 data "aws_iam_role" "codepipeline_role" {
   name = "demo_meetup_codepipeline_role"
