eb34b5afd52e0e435da88a2b5e0b27b45e7856bc
nugowe <nosa.ugowe@yahoo.com>
2022-06-10 16:27:26 -0500

    made changes to tf files

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    region  = "us-west-2"
+    region  = "us-east-2"
     bucket  = "demo-meetup-awspipeline-tools-state-files"
     key     = "demo-meetup-awspipeline-tools-state-files"
     encrypt = true #AES-256encryption
