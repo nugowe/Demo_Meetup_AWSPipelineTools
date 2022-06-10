a6b51f4834dace81423a5c858ff248d29af99b49
nugowe <nosa.ugowe@yahoo.com>
2022-06-10 10:32:02 -0500

    made changes to iam.tf files

--- terraform/cloudbuild_iam.tf
+++ terraform/cloudbuild_iam.tf
@@ -1,27 +1,33 @@
 
-resource "aws_iam_role" "example" {
+data "aws_iam_role" "codebuild" {
   name = "meetup_demo"
+}
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+
+
+#resource "aws_iam_role" "example" {
+  #name = "meetup_demo"
+
+  #assume_role_policy = <<EOF
+#{
+  #"Version": "2012-10-17",
+  #"Statement": [
     {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "codebuild.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
+      #"Effect": "Allow",
+      #"Principal": {
+        #"Service": "codebuild.amazonaws.com"
+      #},
+      #"Action": "sts:AssumeRole"
+    #}
+  #]
+#}
+#EOF
+#}
 
 # Create an IAM role policy for CodeBuild to use implicitly
 resource "aws_iam_role_policy" "codebuild_iam_role_policy" {
   name = "code_build_iam_policy_meetup_demo"
-  role = aws_iam_role.example.name
+  role = data.aws_iam_role.codebuild.name
 
 policy = jsonencode({
     "Version": "2012-10-17",
@@ -94,48 +100,56 @@ policy = jsonencode({
 }
 
 
-# Create IAM role for Terraform builder to assume
-resource "aws_iam_role" "tf_iam_assumed_role" {
+data "aws_iam_role" "tf_iam_assumed_role" {
   name = "TerraformAssumedIamRole"
-
-assume_role_policy = jsonencode({
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "${aws_iam_role.example.arn}"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-  })
 }
+
+data "aws_iam_role" "tf_iam_assumed_policy" {
+  name = "TerraformAssumedIamPolicy"
+}
+
+# Create IAM role for Terraform builder to assume
+#resource "aws_iam_role" "tf_iam_assumed_role" {
+  #name = "TerraformAssumedIamRole"
+
+#assume_role_policy = jsonencode({
+  #"Version": "2012-10-17",
+  #"Statement": [
+    #{
+      #"Effect": "Allow",
+      #"Principal": {
+        #"AWS": "${aws_iam_role.example.arn}"
+      #},
+      #"Action": "sts:AssumeRole"
+    #}
+  #]
+  #})
+#}
 
  
 
 # Create broad IAM policy Terraform to use to build, modify resources
-resource "aws_iam_policy" "tf_iam_assumed_policy" {
-  name = "TerraformAssumedIamPolicy"
-  policy = jsonencode({
-    "Version": "2012-10-17",
-  "Statement": [
+#resource "aws_iam_policy" "tf_iam_assumed_policy" {
+  #name = "TerraformAssumedIamPolicy"
+  #policy = jsonencode({
+    #"Version": "2012-10-17",
+  #"Statement": [
     {
-      "Sid": "AllowAllPermissions",
-      "Effect": "Allow",
-      "Action": [
-        "*"
-      ],
-      "Resource": "*"
-    }
-  ]
-  })
-}
+      #"Sid": "AllowAllPermissions",
+      #"Effect": "Allow",
+      #"Action": [
+        #"*"
+      #],
+      #"Resource": "*"
+    #}
+  #]
+  #})
+#}
 
 
 
 # Attach IAM assume role to policy
 resource "aws_iam_role_policy_attachment" "tf_iam_attach_assumed_role_to_permissions_policy" {
-  role       = aws_iam_role.tf_iam_assumed_role.name
-  policy_arn = aws_iam_policy.tf_iam_assumed_policy.arn
+  role       = data.aws_iam_role.tf_iam_assumed_role.name
+  policy_arn = data.aws_iam_policy.tf_iam_assumed_policy.arn
 }
--- terraform/codepipeline_iam.tf
+++ terraform/codepipeline_iam.tf
@@ -1,26 +1,30 @@
-resource "aws_iam_role" "codepipeline_role" {
-  name = "demo_meetup_codepipeline_role"
+#resource "aws_iam_role" "codepipeline_role" {
+  #name = "demo_meetup_codepipeline_role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "codepipeline.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+  #assume_role_policy = <<EOF
+#{
+  #"Version": "2012-10-17",
+  "#Statement": [
+    #{
+      #"Effect": "Allow",
+      "#Principal": {
+        #"Service": "codepipeline.amazonaws.com"
+      #},
+      #"Action": "sts:AssumeRole"
+    #}
+  #]
+#}
+#EOF
+#}
+
+data "aws_iam_role" "codepipeline_role" {
+  name = "demo_meetup_codepipeline_role"
 }
 
 
 resource "aws_iam_role_policy" "codepipeline_policy" {
   name = "codepipeline_policy"
-  role = aws_iam_role.codepipeline_role.id
+  role = data.aws_iam_role.codepipeline_role.id
 
 policy = jsonencode({
   "Version": "2012-10-17",
