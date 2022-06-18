5f37b853af62d8d55bd9fb9b173a76c86f6b53d4
Nosa Ugowe <nosa.ugowe@yahoo.com>
2022-06-18 13:25:42 -0500

    Update codecommit.tf

--- terraform/codecommit.tf
+++ terraform/codecommit.tf
@@ -1,4 +1,7 @@
 resource "aws_codecommit_repository" "demo" {
+  depends_on = [
+    aws_ecr_repository.demo.arn
+  ]
   repository_name = "epltable-repository"
   description     = "Demo Repository"
 }
